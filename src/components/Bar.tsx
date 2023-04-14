bc1b69352b39f60d6c956f84435e0588cbedcef3
shivam-raj-4 <shivamraj.cs17@bmsce.ac.in>
2023-04-15 02:45:09 +0530

    optimizing bar chart

--- src/components/Bar.tsx
+++ src/components/Bar.tsx
@@ -2,27 +2,26 @@ import ReactECharts from "echarts-for-react";
 import data from "../data/Wine-Data.json";
 
 function Bar() {
-  // Gathering "Alcohol" category on horizontal axis
-  const alcoholType: number[] = [];
-  data.forEach((value) => {
-    // Condition wise storing the distinct alcohol category
-    if (!alcoholType.includes(value.Alcohol)) alcoholType.push(value.Alcohol);
-  });
-  const xAxis = alcoholType;
-
-  // Keeping average of "Malic Acid" for each class on vertical axis
+  const xAxis: number[] = [];
   const yAxis: number[] = [];
-  alcoholType.forEach((value) => {
-    // Grouping different alcohol type by filtering the data
-    const alcoholCategorized = data.filter((item) => item.Alcohol === value);
-    let sumMalicAcid = 0;
-    alcoholCategorized.forEach((category) => {
-      // sum of Malic Acid from grouped alcohol type
-      sumMalicAcid += category["Malic Acid"];
-    });
-    // Taking out the average of Malic Acid
-    const avgMalicAcid = sumMalicAcid / alcoholCategorized.length;
-    yAxis.push(avgMalicAcid);
+
+  // Initializing both as Map
+  let malicAcidSum = new Map();
+  let alcoholFrequency = new Map();
+  data.forEach((value) => {
+    // Storing distinct alcohol categories as the key for both maps
+    let alcoholType = value.Alcohol;
+    // Storing sum of malic acid from grouped alcohol type in value of this map
+    malicAcidSum.set(alcoholType, (malicAcidSum.get(alcoholType) || 0) + value["Malic Acid"]);
+    // Storing count of alcohol type
+    alcoholFrequency.set(alcoholType, (alcoholFrequency.get(alcoholType) || 0) + 1);
+  });
+  
+  malicAcidSum.forEach(function(value, key){
+    // Pushing different alcohol class on horizontal axis
+    xAxis.push(key);
+    // Pushing average of malic acid for each alcohol class on vertical axis
+    yAxis.push(value/alcoholFrequency.get(key));
   });
 
   return (
